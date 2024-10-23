48832305ab4836feda298124a8cff7a42f8774ed
naomai <lulu@nieznani-sprawcy.pl>
2024-10-23 13:57:07 +0200

    increase session lifetime to 60 days

--- config/session.php
+++ config/session.php
@@ -32,7 +32,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 60 * 24 * 60), // 60 days
 
     'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
 
