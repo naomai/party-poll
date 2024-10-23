49dca3cdb4804c77afdbbf5ebd57ecf6aa5792f4
naomai <lulu@nieznani-sprawcy.pl>
2024-10-23 14:52:17 +0200

    add tests for poll

--- tests/Feature/Authz/PollTest.php
+++ tests/Feature/Authz/PollTest.php
@@ -8,21 +8,76 @@ use Database\Seeders\PollSeeder;
 use Database\Seeders\UserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class PollTest extends TestCase {
     use RefreshDatabase;
 
-    public function test_access_single_question(): void {
+    public function setUp(): void {
+        parent::setUp();
+        
         $this->seed(UserSeeder::class);
         $this->seed(PollSeeder::class);
         
+    }
+
+    public function test_list_poll(): void {
         $poll = Poll::all()->random();
-        $user = $poll->users->random();
+        $user = $this->getPollMuggle($poll);
+
+        $pollCount = $user->polls->count();
+
+        $this->actingAs($user)
+            ->getJson(route('polls.index'))
+            ->assertStatus(200)
+            ->assertJsonCount($pollCount)
+            ->assertJsonStructure([
+                '*' => [
+                    'id',
+                    'title',
+                    'owner' => ['id', 'name'],
+                    'links' => ['summary'],
+                ],
+            ]);
+    }
+
+    public function test_access_poll_info_muggle(): void {
+        $poll = Poll::all()->random();
+        $user = $this->getPollMuggle($poll);
+
+        $this->actingAs($user)
+            ->getJson(route('polls.show', ['poll'=>$poll->id]))
+            ->assertStatus(200);
+    }
+
+    public function test_access_single_question_muggle(): void {
+        $poll = Poll::all()->random();
+        $user = $this->getPollMuggle($poll);
         $question = $poll->questions->random();
 
         $this->actingAs($user)
-            ->get(route('poll.question.get', $question->id))
+            ->getJson(route('poll.question.get', ['poll'=>$poll->id, 'question'=>$question->id]))
             ->assertStatus(200);
+    }
+
+
+
+    private function getPollMuggle(Poll $poll) : User {
+        $user = $poll->users()->where([
+            ['can_modify_poll', '=', 'false'],
+            ['can_control_flow', '=', 'false'],
+            ['can_see_progress', '=', 'false'],
+        ])->get()->random();
+        return $user;
+    }
+
+    private function getPollWizard(Poll $poll) : User {
+        $user = $poll->users
+            ->  where('can_modify_poll', '=', 'true')
+            ->orWhere('can_control_flow', '=', 'true')
+            ->orWhere('can_see_progress', '=', 'true')
+            ->get()->random();
+        return $user;
     }
 }
