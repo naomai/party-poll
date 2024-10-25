28de3b4f3dd07811c03f080d5e88e526021a0433
naomai <lulu@nieznani-sprawcy.pl>
2024-10-25 12:39:17 +0200

    fix: create missing poll mgmt for web

---
+++ app/Http/Controllers/Web/PollManagementController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PollInfoUpdateRequest;
+use App\Models\Poll;
+use App\Services\PollManagementService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use Inertia\Inertia;
+use Inertia\Response as InertiaResponse;
+
+class PollManagementController extends Controller {
+    public function index(PollManagementService $manage): InertiaResponse {
+        $pollList = $manage->index();
+        return Inertia::render("PollIndex/Index", [
+            'polls' => $pollList,
+        ]);
+    }
+
+    public function store(
+        PollInfoUpdateRequest $request, 
+        PollManagementService $manage
+    ) : RedirectResponse {
+
+        $pollSummary = $manage->store($request->validated());
+       
+        return to_route("polls.show", [
+            'poll'=>$pollSummary->id
+        ], 303);
+    }
+    
+    public function show(
+        Poll $poll, 
+        PollManagementService $manage
+    ): InertiaResponse {
+
+        $pollSummary = $manage->show($poll);
+        return Inertia::render("Poll/Summary", [
+            'poll' => $pollSummary
+        ]);
+    }
+
+    public function update(
+        Poll $poll, 
+        PollInfoUpdateRequest $request, 
+        PollManagementService $manage
+    ): JsonResponse {
+        
+        $pollSummary = $manage->update($poll, $request->validated());
+        return response()->json($pollSummary);
+    }
+    
+}
