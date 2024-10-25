20aa2340d9b9106739bfba239764d27631b178c1
naomai <lulu@nieznani-sprawcy.pl>
2024-10-25 12:36:10 +0200

    fe: poll list fix list style, consume create event

--- resources/js/Pages/PollIndex/Index.vue
+++ resources/js/Pages/PollIndex/Index.vue
@@ -45,11 +45,11 @@ const hasPolls = computed(()=>
                     class="overflow-hidden bg-white shadow-sm sm:rounded-lg flex justify-center gap-6 flex-wrap"
                 >
                     <div v-if="!hasPolls" class="text-gray-400 text-center px-6 py-6 w-full">You don't have any polls yet. Try creating one!</div>
-                    <ul v-if="hasPolls" role="list" class="divide-y  divide-gray-100 text-lg">
+                    <ul v-if="hasPolls" role="list" class="w-full divide-y  divide-gray-100 text-lg">
                         
                         <li v-for="poll in polls.data" class="flex justify-between gap-x-6 py-5">
                             <div class="flex min-w-0 gap-x-4">
-                                <div class="min-w-0 flex-auto">
+                                <div class="pl-6 min-w-0 flex-auto">
                                     <p class="text-sm font-semibold leading-6 text-gray-900">{{ poll.title }}</p>
                                     <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ poll.participant_count }} ppl</p>
                                 </div>
@@ -63,7 +63,7 @@ const hasPolls = computed(()=>
             </div>
         </div>
         <Modal :show="inPollInfoEditor">
-            <PollPropertiesForm :poll="pollToEdit"></PollPropertiesForm>
+            <PollPropertiesForm :poll="pollToEdit" @create="inPollInfoEditor=false"></PollPropertiesForm>
         </Modal>
     </AuthenticatedLayout>
 </template>
