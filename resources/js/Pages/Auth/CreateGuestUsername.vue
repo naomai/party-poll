79da5dc0afe358694d1dd28318191eee4ea00ce6
naomai <lulu@nieznani-sprawcy.pl>
2024-10-25 12:39:54 +0200

    fe: create onboarding page

---
+++ resources/js/Pages/Auth/CreateGuestUsername.vue
@@ -0,0 +1,57 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import { Head, Link, useForm } from '@inertiajs/vue3';
+
+const form = useForm({
+    name: '',
+});
+
+const submit = () => {
+    form.put(route('guest_upgrade'));
+};
+</script>
+
+<template>
+    <GuestLayout>
+        <Head title="Your name" />
+
+        <form @submit.prevent="submit">
+            <div>
+                <InputLabel for="name" value="Name" />
+
+                <TextInput
+                    id="name"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.name"
+                    required
+                    autofocus
+                    autocomplete="name"
+                />
+
+                <InputError class="mt-2" :message="form.errors.name" />
+            </div>
+
+            <div class="mt-4 flex items-center justify-end">
+                <Link
+                    :href="route('login')"
+                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                    Already registered?
+                </Link>
+
+                <PrimaryButton
+                    class="ms-4"
+                    :class="{ 'opacity-25': form.processing }"
+                    :disabled="form.processing"
+                >
+                    OK
+                </PrimaryButton>
+            </div>
+        </form>
+    </GuestLayout>
+</template>
