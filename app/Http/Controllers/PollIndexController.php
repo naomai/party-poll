5b25cf6a924f53af9c2a098687322daf3be439bd
naomai <lulu@nieznani-sprawcy.pl>
2024-10-25 10:06:47 +0200

    use pollmgmt service in web controller

--- app/Http/Controllers/PollIndexController.php
+++ app/Http/Controllers/PollIndexController.php
@@ -2,26 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\PollBasicInfoResource;
-use App\Models\Poll;
-use App\Models\User;
+use App\Services\PollManagementService;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class PollIndexController extends Controller {
     use AuthorizesRequests;
 
-    public function view(User $user) {
-        $this->authorize('index', Poll::class);
-        $list = $this->getAllPollsForUser()->get();
+    public function view(PollManagementService $manage) {
+        $pollList = $manage->index();
         return Inertia::render("PollIndex/Index", [
-            'polls' => PollBasicInfoResource::collection($list),
+            'polls' => $pollList,
         ]);
     }
 
-    private function getAllPollsForUser() {
-        return Poll::whereRelation("users", 'user_id', "=", Auth::user()->id);
-    }
 }
