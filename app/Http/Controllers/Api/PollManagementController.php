5c339a6b56e11a31eeec1b2e59ee744434ce0c1e
naomai <lulu@nieznani-sprawcy.pl>
2024-10-25 10:09:30 +0200

    drop auth trait from controllers auth is moved into services

--- app/Http/Controllers/Api/PollManagementController.php
+++ app/Http/Controllers/Api/PollManagementController.php
@@ -6,13 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\PollInfoUpdateRequest;
 use App\Models\Poll;
 use App\Services\PollManagementService;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\JsonResponse;
 
-
 class PollManagementController extends Controller {
-    use AuthorizesRequests;
-
     public function index(PollManagementService $manage): JsonResponse {
         $index = $manage->index();
         return response()->json($index);   
--- app/Http/Controllers/PollIndexController.php
+++ app/Http/Controllers/PollIndexController.php
@@ -3,12 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Services\PollManagementService;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Inertia\Inertia;
 
 class PollIndexController extends Controller {
-    use AuthorizesRequests;
-
     public function view(PollManagementService $manage) {
         $pollList = $manage->index();
         return Inertia::render("PollIndex/Index", [
