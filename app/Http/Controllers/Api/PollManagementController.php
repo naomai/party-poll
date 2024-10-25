4e210ef2bf2a1121962fb41ff44618ea2e4ab764
naomai <lulu@nieznani-sprawcy.pl>
2024-10-25 09:48:04 +0200

    delegate logic from Controllers to Services share code betweeen api and web controllers

--- app/Http/Controllers/Api/PollManagementController.php
+++ app/Http/Controllers/Api/PollManagementController.php
@@ -4,56 +4,33 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PollInfoUpdateRequest;
-use App\Http\Resources\PollBasicInfoResource;
-use App\Http\Resources\PollSummaryResource;
 use App\Models\Poll;
-use App\Models\User;
-use App\Services\PollStateService;
-use Illuminate\Support\Facades\Gate;
+use App\Services\PollManagementService;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Http\Client\Response;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Collection;
+
 
 class PollManagementController extends Controller {
     use AuthorizesRequests;
 
-    public function index(): JsonResponse {
-        $this->authorize('index', Poll::class);
-        $list = $this->getAllPollsForUser()->get();
-
-        return response()
-            ->json(PollBasicInfoResource::collection($list));
+    public function index(PollManagementService $manage): JsonResponse {
+        $index = $manage->index();
+        return response()->json($index);   
     }
 
-    public function store(PollInfoUpdateRequest $request) : JsonResponse {
-        $this->authorize('store', Poll::class);
-
-        $poll = Poll::make($request->validated());
-        $poll->owner_id = Auth::user()->id;
-        $poll->save();
-        return response()->json($poll);
+    public function store(PollInfoUpdateRequest $request, PollManagementService $manage) : JsonResponse {
+        $pollSummary = $manage->store($request->validated());
+        return response()->json($pollSummary);
     }
-
-    public function show(Poll $poll): JsonResponse {
-        $this->authorize('view', $poll);
-        return response()
-            ->json(new PollSummaryResource($poll));
+    
+    public function show(Poll $poll, PollManagementService $manage): JsonResponse {
+        $pollSummary = $manage->show($poll);
+        return response()->json($pollSummary);
     }
 
     public function update(PollInfoUpdateRequest $request, Poll $poll) {
-        $this->authorize('update', Poll::class);
-        $poll->update($request->validated());
-        return response()->json($poll);
+        $pollSummary = $poll->update($request->validated());
+        return response()->json($pollSummary);
     }
-
-    public function destroy(string $id) {
-        //
-    }
-
-    private function getAllPollsForUser() {
-        return Poll::whereRelation("users", 'user_id', "=", Auth::user()->id);
-    }
+    
 }
---
+++ app/Services/PollManagementService.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Services;
+
+use App\Http\Resources\PollBasicInfoResource;
+use App\Http\Resources\PollSummaryResource;
+use App\Models\Poll;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+
+class PollManagementService {
+    public function index(): ResourceCollection {
+        Gate::authorize('index', Poll::class);
+
+        $list = $this->getAllPollsForUser()->get();
+        return PollBasicInfoResource::collection($list);
+    }
+
+    public function store(Array $pollData) : JsonResource {
+        Gate::authorize('store', Poll::class);
+
+        $poll = Poll::make($pollData);
+        $poll->owner_id = Auth::user()->id;
+        $poll->save();
+        return new PollSummaryResource($poll);
+    }
+
+    public function show(Poll $poll): JsonResource {
+        Gate::authorize('view', $poll);
+
+        return new PollSummaryResource($poll);
+    }
+
+    public function update(Array $pollData, Poll $poll): JsonResource {
+        Gate::authorize('update', Poll::class);
+
+        $poll->update($pollData);
+        return new PollSummaryResource($poll);
+    }
+
+    public function destroy(string $id) {
+        //
+    }
+
+    private function getAllPollsForUser() {
+        return Poll::whereRelation("users", 'user_id', "=", Auth::user()->id);
+    }
+}
