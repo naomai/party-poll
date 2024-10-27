9ea4dad31172ca44a6d567d7e8bd2078ca06c348
naomai <lulu@nieznani-sprawcy.pl>
2024-10-27 10:30:16 +0100

    fe: use unwrapped json, add poll link in list

--- resources/js/Pages/PollIndex/Index.vue
+++ resources/js/Pages/PollIndex/Index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import ListAddButton from '@/Components/ListAddButton.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 import { computed, reactive } from 'vue';
 import PollPropertiesForm from '../PollManagement/PollPropertiesForm.vue';
 import Modal from '@/Components/Modal.vue';
@@ -23,19 +23,19 @@ const pollToEdit = ref({
 });
 
 const hasPolls = computed(()=>
-    props.polls.data.length > 0
+    props.polls.length > 0
 )
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <Head title="Your polls" />
 
     <AuthenticatedLayout>
         <template #header>
             <h2
                 class="text-xl font-semibold leading-tight text-gray-800"
             >
-                Dashboard
+            Your polls
             </h2>
         </template>
 
@@ -47,13 +47,15 @@ const hasPolls = computed(()=>
                     <div v-if="!hasPolls" class="text-gray-400 text-center px-6 py-6 w-full">You don't have any polls yet. Try creating one!</div>
                     <ul v-if="hasPolls" role="list" class="w-full divide-y  divide-gray-100 text-lg">
                         
-                        <li v-for="poll in polls.data" class="flex justify-between gap-x-6 py-5">
-                            <div class="flex min-w-0 gap-x-4">
-                                <div class="pl-6 min-w-0 flex-auto">
-                                    <p class="text-sm font-semibold leading-6 text-gray-900">{{ poll.title }}</p>
-                                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ poll.participant_count }} ppl</p>
+                        <li v-for="poll in polls" class="flex justify-between gap-x-6 py-5">
+                            <Link :href="route('polls.show', poll.id)">
+                                <div class="flex min-w-0 gap-x-4">
+                                    <div class="pl-6 min-w-0 flex-auto">
+                                        <p class="text-sm font-semibold leading-6 text-gray-900">{{ poll.title }}</p>
+                                        <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ poll.participant_count }} ppl</p>
+                                    </div>
                                 </div>
-                            </div>
+                            </Link>
                         </li>
                     </ul>
                     <div class="self-center py-6">
