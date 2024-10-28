3b57cef3c45231cdf6038a94252c97d172f7be2f
naomai <lulu@nieznani-sprawcy.pl>
2024-10-28 23:53:14 +0100

    fe: add Circular Slider and Star Rating deps

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import Vue3Slider from 'vue3-slider';
+import VueStarRating from 'vue-star-rating'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -24,4 +26,5 @@ createInertiaApp({
     progress: {
         color: '#4B5563',
     },
+    components: { Vue3Slider, VueStarRating }
 });
