f1baf0c9d14db191751f23a9ce9c73b05490e205
naomai <lulu@nieznani-sprawcy.pl>
2024-10-28 23:57:53 +0100

    fe: add allowed actions icon

---
+++ resources/js/Pages/Poll/Partials/AllowedActions.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+const props = defineProps({
+    participation: {
+        type: Object,
+    },
+});
+</script>
+
+<template>
+    <div 
+        class="rounded-full bg-gray-700 inline-flex" 
+        style="width: 30px; height: 30px "
+        :class="{'opacity-50': !participation.can_answer}"
+    >a</div>
+    <div 
+        class="rounded-full bg-gray-700 inline-flex" 
+        style="width: 30px; height: 30px "
+        :class="{'opacity-50': !participation.can_modify_poll}"
+    >b</div>
+    <div 
+        class="rounded-full bg-gray-700 inline-flex" 
+        style="width: 30px; height: 30px "
+        :class="{'opacity-50': !participation.can_control_flow}"
+    >c</div>
+    <div 
+        class="rounded-full bg-gray-700 inline-flex" 
+        style="width: 30px; height: 30px "
+        :class="{'opacity-50': !participation.can_see_progress}"
+    >d</div>
+</template>
