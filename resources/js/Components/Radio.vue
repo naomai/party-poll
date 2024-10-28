0519942d965104400976dcc237489a0d9500b03b
naomai <lulu@nieznani-sprawcy.pl>
2024-10-28 23:50:17 +0100

    fe: create radio button

---
+++ resources/js/Components/Radio.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { computed } from 'vue';
+
+const emit = defineEmits(['update:checked']);
+
+const props = defineProps({
+    value: {
+        type: String,
+    },
+    selectedId: {
+        type: String,
+    },
+    id: {
+        type: String,
+    }, 
+    group: {
+        type: String,
+    }
+});
+
+const proxyChecked = computed({
+    get() {
+        return props.selectedId == props.id;
+    },
+
+    set(val) {
+        emit('update:selectedId', val);
+    },
+});
+</script>
+
+<template>
+    <input
+        type="radio"
+        :value="value"
+        :name="group"
+        :id="id"
+        v-model="proxyChecked"
+        class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+    />
+</template>
