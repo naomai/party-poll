9f0de7e24e2d79bb7a5c2ffe4e32b3b8cff04c21
naomai <lulu@nieznani-sprawcy.pl>
2024-10-29 13:59:37 +0100

    fix: redirect loop when validation fails (inertia)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,14 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
-            \App\Http\Middleware\HandleInertiaRequests::class,
-            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-        ]);
-
-        //
-    })
-    ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
     })
     ->withMiddleware(function (Middleware $middleware) {
@@ -27,7 +19,16 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
             \Illuminate\Session\Middleware\StartSession::class,
         ]);
+    })    
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->web(append: [
+            \App\Http\Middleware\HandleInertiaRequests::class,
+            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+        ]);
+
+        //
     })
+
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
