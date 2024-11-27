612c19e4e403bceb90ea8b59c07f8e603bd0fddc
naomai <lulu@nieznani-sprawcy.pl>
2024-11-27 13:17:25 +0100

    feat: validate response params in question edit

--- app/Http/Requests/QuestionStoreRequest.php
+++ app/Http/Requests/QuestionStoreRequest.php
@@ -16,6 +16,17 @@ class QuestionStoreRequest extends FormRequest {
             'question' => ['required', 'string', 'max:255'],
             'type' => ['required', Rule::in(['input','range','rating','select'])],
             'response_params' => ['required', 'array'],
+            
+            'response_params.type' => ['exclude_unless:type,input','required', Rule::in(['text','pass','longtext'])],
+            'response_params.max_length' => ['exclude_unless:type,input','required', 'integer', "between: 0, 1000"],
+            
+            'response_params.min' => ['exclude_unless:type,range','required', 'integer'],
+            'response_params.max' => ['exclude_unless:type,range','required', 'integer', "gt:response_params.min"],
+
+            'response_params.options' => ['exclude_unless:type,select',"required", "array", "between:1,20"],
+            'response_params.options.*.caption' => ['exclude_unless:type,select',"present", "string", "nullable", "between:0,500"],
+            'response_params.max_selected' => ['exclude_unless:type,select','required', 'integer', "between: 0, 20"],
+
 
         ];
     }
--- app/Http/Requests/QuestionUpdateRequest.php
+++ app/Http/Requests/QuestionUpdateRequest.php
@@ -17,6 +17,16 @@ class QuestionUpdateRequest extends FormRequest {
             'poll_sequence_id' => ['integer'],
             'type' => ['required', Rule::in(['input','range','rating','select'])],
             'response_params' => ['array'],
+            
+            'response_params.type' => ['exclude_unless:type,input','required', Rule::in(['text','pass','longtext'])],
+            'response_params.max_length' => ['exclude_unless:type,input','required', 'integer', "between: 0, 1000"],
+            
+            'response_params.min' => ['exclude_unless:type,range','required', 'integer'],
+            'response_params.max' => ['exclude_unless:type,range','required', 'integer', "gt:response_params.min"],
+
+            'response_params.options' => ['exclude_unless:type,select',"required", "array", "between:1,20"],
+            'response_params.options.*.caption' => ['exclude_unless:type,select',"present", "string", "nullable", "between:0,500"],
+            'response_params.max_selected' => ['exclude_unless:type,select','required', 'integer', "between: 0, 20"],
 
         ];
     }
