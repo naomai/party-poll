76b31868e58884b5a7f4ce5208588967fe196abd
naomai <lulu@nieznani-sprawcy.pl>
2024-11-27 13:21:56 +0100

    feat: add range editor

---
+++ resources/js/Pages/Poll/Partials/EditRange.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { computed, reactive, ref } from 'vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { useForm } from '@inertiajs/vue3';
+import IconTextInput from '@/Components/IconTextInput.vue';
+import TextInput from '@/Components/TextInput.vue';
+
+
+const question = defineModel('question');
+
+</script>
+
+<template>
+    From <TextInput v-model="question.response_params.min" type="number" required :max="question.response_params.max-1" size="4" />
+    to <TextInput v-model="question.response_params.max" type="number" required :min="question.response_params.min+1" size="4" />
+</template>
