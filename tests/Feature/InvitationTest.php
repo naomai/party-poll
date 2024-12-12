0c63a0aa81174840cab221082ea437a4af3aa59a
naomai <lulu@nieznani-sprawcy.pl>
2024-12-12 14:33:04 +0100

    tests(invitation): invalid/expired, start conditions

--- tests/Feature/InvitationTest.php
+++ tests/Feature/InvitationTest.php
@@ -10,8 +10,19 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Inertia\Testing\AssertableInertia;
 use Tests\TestCase;
 
-class InvitationTest extends TestCase
-{
+class InvitationTest extends TestCase {
+
+    public function test_start_conditions() : void {
+        [$poll, $user] = $this->_prepareInvitationConditions();
+
+        $this->assertTrue($poll->enable_link_invite, "test poll has invitations enabled");
+        $this->assertTrue($poll->close_after_start, "test poll closes invitations after starting");
+        $this->assertStringStartsWith("lorem-ipsum-", $poll->access_link_token, "test poll has valid invite token");
+        
+        $this->assertInstanceOf(User::class, $user, "test user is being created");
+        $this->assertIsNumeric($user->id, "test user record is in database");
+    }
+
     public function test_accept_invitation(): void {
         [$poll, $user] = $this->_prepareInvitationConditions();
         $invitationToken = $poll->access_link_token;
@@ -43,9 +54,56 @@ class InvitationTest extends TestCase
                 'poll'=>$poll->id, 
                 'accesskey'=>$invitationToken,
             ]))
+            ->assertGone()
+            ->assertInertia(fn (AssertableInertia $page) => $page
+                ->component('Invite/Denied')
+                ->where("status", 2)
+                ->has("poll")
+            )
+        ;
+    }
+
+    public function test_accept_invitation_fail_on_invalid_token(): void {
+        [$poll, $user] = $this->_prepareInvitationConditions();
+        $invitationToken = "lorem-ipsum-invalid-invalid";
+
+        $this->actingAs($user)
+            ->get(route('invite.view', [
+                'poll'=>$poll->id, 
+                'accesskey'=>$invitationToken,
+            ]))
             ->assertForbidden()
             ->assertInertia(fn (AssertableInertia $page) => $page
                 ->component('Invite/Denied')
+                ->where("status", 1)
+                ->missing("poll")
+            )
+        ;
+
+        $invitationToken = "invalid-invalid-invalid";
+
+        $this->actingAs($user)
+            ->get(route('invite.view', [
+                'poll'=>$poll->id, 
+                'accesskey'=>$invitationToken,
+            ]))
+            ->assertForbidden()
+            ->assertInertia(fn (AssertableInertia $page) => $page
+                ->component('Invite/Denied')
+                ->where("status", 1)
+                ->missing("poll")
+            )
+        ;
+
+        $this->actingAs($user)
+            ->get(route('invite.view', [
+                'poll'=>$poll->id,
+            ]))
+            ->assertForbidden()
+            ->assertInertia(fn (AssertableInertia $page) => $page
+                ->component('Invite/Denied')
+                ->where("status", 1)
+                ->missing("poll")
             )
         ;
     }
@@ -70,6 +128,7 @@ class InvitationTest extends TestCase
     private function _prepareInvitationConditions(): array {
         [$poll, $user] = Helpers::createPollWithAdmin([
             'enable_link_invite' => true,
+            'close_after_start' => true,
         ]);
 
         $otherUser = User::factory()->create();
